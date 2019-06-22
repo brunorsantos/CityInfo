b6f02665d9d807470c80117178368ec3c1396b0b
Bruno Santos <bruno.santos@pottencial.com.br>
2019-06-22 18:18:00 -0300

    migration

--- CityInfo.Api/Entities/PointOfInterests.cs
+++ CityInfo.Api/Entities/PointOfInterests.cs
@@ -16,6 +16,9 @@ namespace CityInfo.Api.Entities
         [MaxLength(50)]
         public string Name { get; set; }
 
+        [MaxLength(200)]
+        public string Description { get; set; }
+
         [ForeignKey("CityId")]
         public City city { get; set; }
 
