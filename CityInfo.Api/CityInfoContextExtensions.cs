b9b9d7a4b14e0f5138f2c3f62516ed1b984996e5
Bruno Santos <bruno.santos@pottencial.com.br>
2019-06-27 21:29:17 -0300

    Seed (old way)

---
+++ CityInfo.Api/CityInfoContextExtensions.cs
@@ -0,0 +1,55 @@
+﻿using CityInfo.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api
+{
+    public static class CityInfoContextExtensions
+    {
+        public static void EnsureSeedDataForContext(this CityInfoContext context)
+        {
+
+            if (context.Cities.Any())
+            {
+                return;
+            }
+
+            var cities = new List<City>()
+            {
+                new City()
+                {
+                    Name = "Test city",
+                    Description = "DEscription test1",
+                    pointsOfInterests = new List<PointOfInterests>()
+                    {
+                        new PointOfInterests()
+                        {
+                            Name = "Point of interest 1",
+                            Description = "Point of interest description 1"
+                        }
+                    }
+
+                },
+                  new City()
+                {
+                    Name = "Test city2",
+                    Description = "DEscription test2",
+                    pointsOfInterests = new List<PointOfInterests>()
+                    {
+                        new PointOfInterests()
+                        {
+                            Name = "Point of interest 2",
+                            Description = "Point of interest description 2"
+                        }
+                    }
+
+                }
+            };
+
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
+        }
+    }
+}
